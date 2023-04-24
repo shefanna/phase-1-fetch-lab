24fdb51e807f9582de08aae297a676eeb41e597e
shefinannathampy <shefin04annathampy94@gmail.com>
2023-04-23 20:54:17 -0400

    fetch lab

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
     .then(response => response.json())
     .then(data => renderBooks(data))
-    
+    .catch(error => console.log(error.message));
 }
 
 function renderBooks(books) {
